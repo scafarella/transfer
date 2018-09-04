b7a95e861d53ec0089913c9375a1da94bfcaac39
stefano cafarella <stefano.cafarella@matchesfashion.com>
2018-09-04 14:33:11 +0100

    starting h2

--- src/main/java/com/revolut/transfer/App.java
+++ src/main/java/com/revolut/transfer/App.java
@@ -4,6 +4,11 @@ import com.revolut.transfer.controllers.TransferController;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.h2.server.web.DbStarter;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,13 +18,18 @@ public class App {
         Server jettyServer = new Server(8080);
         jettyServer.setHandler(context);
 
+        context.addEventListener(new DbStarter());
+
         ServletHolder jerseyServlet = context.addServlet(
                 org.glassfish.jersey.servlet.ServletContainer.class, "/*");
         jerseyServlet.setInitOrder(0);
-
         jerseyServlet.setInitParameter(
                 "jersey.config.server.provider.classnames",
                 TransferController.class.getCanonicalName());
+
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("AccountsPU");
+        EntityManager entityManager = factory.createEntityManager();
 
         try {
             jettyServer.start();
