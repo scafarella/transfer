aa18c136f9f8901af7f3fc6211d09cb5701b9f4a
stefano cafarella <stefano.cafarella@matchesfashion.com>
2018-09-04 18:26:07 +0100

    account entity

---
+++ src/main/java/com/revolut/transfer/models/Account.java
@@ -0,0 +1,24 @@
+package com.revolut.transfer.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "accounts")
+public class Account implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/resources/init.sql
@@ -0,0 +1,1 @@
+INSERT INTO accounts (ID) VALUES (1);
